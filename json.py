3f88f73e7dc26393df0dda49d83966181f7768bb
Sujith <sujithreddyaug1998@gmail.com>
2026-03-14 09:40:36 +0530

    sync with the latest changes

--- json.py
+++ json.py
@@ -1,2 +1,8 @@
 '''json code to push into sql database'''
 import json
+
+def json_to_sql(json_file):
+    '''function to convert json to sql'''
+    with open(json_file) as file:
+        data = json.load(file)
+    return data
